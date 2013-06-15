be6c05872794eef8b0f87f9523a63611e0b62b56
Mick Staugaard <mick@staugaard.com>
2013-06-14 23:09:49 -0700

    refactoring and animation support

--- led_strip_wheel.ino
+++ led_strip_wheel.ino
@@ -2,38 +2,38 @@
 #include <avr/pgmspace.h>
 #include "data.h"
 
-Adafruit_NeoPixel strip0 = Adafruit_NeoPixel(15, 5, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(15, 6, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(15, 7, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip3 = Adafruit_NeoPixel(15, 8, NEO_GRB + NEO_KHZ800);
+const byte numberOfStrips = 4;
+const byte numberOfPixels = 15;
+const int resolution      = 256;
+const int topRowOffset    = (resolution * -0.1);
+const int maxTime         = 700;
 
-Adafruit_NeoPixel strips[] = {strip3, strip2, strip1, strip0};
+Adafruit_NeoPixel strips[numberOfStrips] = {
+  Adafruit_NeoPixel(numberOfPixels, 8, NEO_GRB + NEO_KHZ800),
+  Adafruit_NeoPixel(numberOfPixels, 7, NEO_GRB + NEO_KHZ800),
+  Adafruit_NeoPixel(numberOfPixels, 6, NEO_GRB + NEO_KHZ800),
+  Adafruit_NeoPixel(numberOfPixels, 5, NEO_GRB + NEO_KHZ800)
+};
 
 byte stripIndex;
 byte pixelIndex;
 int topRowIndex;
 int rowIndex;
+byte frameNumber;
 prog_uchar *rowOffset;
-
-const byte numberOfStrips      = 4;
-const byte numberOfPixels      = 15;
-const int resolution           = 256;
-const int topRowOffset         = (resolution * -0.1);
-const int maxTime              = 700;
 
 uint8_t r;
 uint8_t g;
 uint8_t b;
 
 volatile unsigned long lastSpeedInterrupt;
-volatile unsigned int millisPerRound;
+volatile unsigned int millisPerRound = 1000;
 
 unsigned int timeSinceTop;
 float position;
 int stripPosition;
 
 void setup() {
-  millisPerRound = 1000;
   lastSpeedInterrupt = millis();
 
   for (stripIndex = 0; stripIndex < numberOfStrips; stripIndex = stripIndex + 1) {
@@ -41,16 +41,21 @@ void setup() {
     strips[stripIndex].show();
   }
 
-  digitalWrite(2, HIGH);
+  digitalWrite(2, HIGH); // this enables the pull-up resistor
   attachInterrupt(0, positionSensorInterrupt, RISING);
 
 //  Serial.begin(9600);
 }
 
+void positionSensorInterrupt() {
+  millisPerRound = millis() - lastSpeedInterrupt;
+  lastSpeedInterrupt = millis();
+}
+
 void loop() {
   timeSinceTop = millis() - lastSpeedInterrupt;
 
-  if (millisPerRound > maxTime || timeSinceTop > maxTime) {
+  if (tooSlow()) {
     for (stripIndex = 0; stripIndex < numberOfStrips; stripIndex = stripIndex + 1) {
       for (pixelIndex = 0; pixelIndex < numberOfPixels; pixelIndex = pixelIndex + 1) {
         strips[stripIndex].setPixelColor(pixelIndex, 255, 0, 0);
@@ -71,6 +76,8 @@ void loop() {
     topRowIndex -= resolution;
   }
 
+  frameNumber = (millis() / 1000) % numberOfFrames;
+
   for (stripIndex = 0; stripIndex < numberOfStrips; stripIndex = stripIndex + 1) {
     stripPosition = topRowIndex + (stripIndex * (resolution / numberOfStrips));
     rowIndex = stripPosition % resolution;
@@ -78,7 +85,7 @@ void loop() {
       rowIndex = resolution - rowIndex;
     }
 
-    rowOffset = data + (numberOfPixels * 3 * rowIndex);
+    rowOffset = frames[frameNumber] + (numberOfPixels * 3 * rowIndex);
 
     for (pixelIndex = 0; pixelIndex < numberOfPixels; pixelIndex = pixelIndex + 1) {
       r = pgm_read_byte_near(rowOffset + (pixelIndex * 3));
@@ -91,8 +98,6 @@ void loop() {
 
 }
 
-void positionSensorInterrupt() {
-  millisPerRound = millis() - lastSpeedInterrupt;
-  lastSpeedInterrupt = millis();
+bool tooSlow() {
+  return millisPerRound > maxTime || timeSinceTop > maxTime;
 }
-
