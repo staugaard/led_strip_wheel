0e94353949f842dd67505b95dd62798432b3bdff
Mick Staugaard <mick@staugaard.com>
2013-07-14 11:46:52 -0700

    slowly getting there

--- app/app.js
+++ app/app.js
@@ -8,11 +8,12 @@ var deadRadius = 1/5;
 var connection = {
   connected: false,
   read: true,
+  line: '',
 
   init: function() {
     connection.log = document.getElementById('log');
     connection.refreshPorts();
-    document.querySelector('#port-selector a').addEventListener('click', connection.refreshPorts());
+    document.querySelector('#port-selector a').addEventListener('click', connection.refreshPorts);
     document.querySelector('#port-selector button').addEventListener('click', function() {
       var select = document.querySelector('#port-selector select');
       connection.connect(select.value);
@@ -20,8 +21,6 @@ var connection = {
   },
 
   refreshPorts: function() {
-    console.log('refresh');
-
     chrome.serial.getPorts(function(ports) {
       console.log(ports);
       var select = document.querySelector('#port-selector select');
@@ -41,8 +40,13 @@ var connection = {
   connect: function(portName) {
     chrome.serial.open(portName, {bitrate: 115200}, function(openInfo) {
       connection.id = openInfo.connectionId;
+      connection.line = '';
+      var wasConnected = connection.connected;
       connection.connected = true;
-      connection._read();
+      connection.read = true;
+      if (!wasConnected) {
+        connection._read();
+      }
     });
   },
 
@@ -64,7 +68,7 @@ var connection = {
   sendDataBuffer: function(buffer, offset) {
     offset = offset || 0;
 
-    chrome.serial.write(connection.id, buffer.slice(offset), function(writeInfo) {
+    chrome.serial.write(connection.id, buffer.slice(offset, Math.min(offset + 32, buffer.byteLength)), function(writeInfo) {
       console.log(writeInfo);
       if (writeInfo.bytesWritten > 0) {
         offset = offset + writeInfo.bytesWritten;
@@ -102,12 +106,31 @@ var connection = {
     connection._readData(expectedLength, function(data) {
       connection.read = true;
       if (callback) {
-        callback(data);
+        callback(data, index);
       } else {
         console.log(data);
       }
     });
 
+  },
+
+  readImages: function(count, offset) {
+    offset = offset || 0;
+
+    if (count == 0 || offset >= count) {
+      return;
+    }
+
+    console.log('getting image ' + offset);
+    connection.readImage(offset, function(optimizedData, index) {
+      console.log('got image ' + index);
+
+      var compactedData = dataOptimizer.compactData(optimizedData);
+      var thumbnail = thumbnails.create(compactedData);
+      thumbnails.add(thumbnail);
+
+      connection.readImages(count, offset + 1);
+    });
   },
 
   _readData: function(length, callback, buffer) {
@@ -120,7 +143,7 @@ var connection = {
         buffer.push(view[i]);
       }
 
-      console.log(readInfo.bytesRead, view.length, buffer.length, length);
+      // console.log(readInfo.bytesRead, view.length, buffer.length, length);
 
       if (buffer.length == length) {
         callback(buffer);
@@ -142,8 +165,24 @@ var connection = {
 
     chrome.serial.read(connection.id, 1, function(readInfo) {
       if (readInfo.bytesRead > 0) {
-        var s = String.fromCharCode.apply(null, new Uint8Array(readInfo.data));
+        var data = new Uint8Array(readInfo.data)
+
+        var s = '';
+        if (data.length != 1 || data[0] != 13) {
+          s = String.fromCharCode.apply(null, data);
+        }
+
         connection.log.value += s;
+        connection.log.scrollTop = connection.log.scrollHeight;
+
+        connection.line += s;
+
+        var match = connection.line.match("^image_count: (\\d+)\n");
+        if (match) {
+          connection.line = '';
+          connection.readImages(Number(match[1]));
+        }
+
         connection._read();
       } else {
         setTimeout(connection._read, 500);
@@ -155,36 +194,200 @@ var connection = {
 connection.init();
 
 
+// Image Rendering
 
+var imageRenderer = {
+  render: function(canvas, compactData, pixelSize) {
+    var renderWidth      = canvas.width;
+    var renderContext    = canvas.getContext('2d');
+    var renderRadius     = renderWidth / 2;
+    var renderDeadRadius = renderRadius * deadRadius;
+    var renderRadi       = [];
+
+    for (var pixel = 0; pixel < pixelCount; pixel++) {
+      renderRadi[pixel]  = ((renderRadius - renderDeadRadius) / pixelCount) * pixel + renderDeadRadius;
+    }
+
+    renderContext.clearRect(0, 0, renderWidth, renderWidth);
+
+    var angle, angleSin, angleCos, x, y, stepData, pixelData;
+    for (var step = 0; step < resolution; step++) {
+      stepData = compactData[step];
+      angle = (step / resolution) * 2 * Math.PI;
+      angleSin = Math.sin(angle);
+      angleCos = Math.cos(angle);
+
+      for (var pixel = 0; pixel < pixelCount; pixel++) {
+        pixelData = stepData[pixel];
+        x = angleCos * renderRadi[pixel] + renderRadius;
+        y = angleSin * renderRadi[pixel] + renderRadius;
+
+        renderContext.fillStyle = 'rgb(' + pixelData[0] + ', ' + pixelData[1] + ', ' + pixelData[2] + ')';
+        renderContext.fillRect(x, y, pixelSize, pixelSize);
+      }
+    }
+  }
+}
+
+
+// Data optimizations
+
+var dataOptimizer = {
+  bytesPerStrip: pixelCount * 3,
+  bytesPerBlock: pixelCount * 3 * 4,
+
+  optimizeData: function(compactData) {
+    var optimizedData = {}
+
+    optimizedData.buffer = new ArrayBuffer(resolution * dataOptimizer.bytesPerBlock);
+    var view = optimizedData.view = new Uint8Array(optimizedData.buffer);
+
+    var stepsBetweenStrips = resolution / 4;
+
+    var strips = [];
+    var stepIndex;
+    var pixelOffset;
+
+    for (var step = 0; step < resolution; step++) {
+
+      for (var strip = 0; strip < 4; strip++) {
+        stepIndex = step + (strip * stepsBetweenStrips);
+        if (stepIndex >= resolution) { stepIndex = stepIndex - resolution; }
+
+        for (var pixel = 0; pixel < pixelCount; pixel++) {
+          pixelOffset = (step * dataOptimizer.bytesPerBlock) + (strip * dataOptimizer.bytesPerStrip) + (pixel * 3);
+          view[pixelOffset + 0] = compactData[stepIndex][pixel][0];
+          view[pixelOffset + 1] = compactData[stepIndex][pixel][1];
+          view[pixelOffset + 2] = compactData[stepIndex][pixel][2];
+        }
+
+      }
+
+    }
+
+    return optimizedData;
+  },
+
+  compactData: function(optimizedData) {
+    var compactedData = [];
+    var stepOffset, pixelOffset;
+
+    for (var step = 0; step < resolution; step++) {
+      stepOffset = step * dataOptimizer.bytesPerBlock;
+      compactedData[step] = [];
+      for (var pixel = 0; pixel < pixelCount; pixel++) {
+        pixelOffset = stepOffset + (pixel * 3);
+        compactedData[step].push([
+          optimizedData[pixelOffset + 0],
+          optimizedData[pixelOffset + 1],
+          optimizedData[pixelOffset + 2]
+        ]);
+      }
+    }
+
+    return compactedData;
+  },
+
+  verifyOptimizedData: function(data) {
+    var stepsBetweenStrips = resolution / 4;
+    var stepIndex;
+    var pixelIndex;
+    var color;
+
+    if (data.length != (resolution * dataOptimizer.bytesPerBlock)) {
+      console.log('data has wrong length');
+      return false;
+    }
+
+    for (var step = 0; step < resolution; step++) {
+      for (var strip = 0; strip < 4; strip++) {
+        stepIndex = step + (strip * stepsBetweenStrips);
+        if (stepIndex >= resolution) { stepIndex = stepIndex - resolution; }
+
+        for (var pixel = 0; pixel < pixelCount; pixel++) {
+          pixelIndex = step * dataOptimizer.bytesPerBlock + strip * dataOptimizer.bytesPerStrip + pixel * 3;
+          color = imageScanner._colorAtPixel(stepIndex, pixel);
+
+          if (data[pixelIndex] != color.r) {
+            console.log('red is wrong');
+            return false;
+          }
+
+          if (data[pixelIndex + 1] != color.g) {
+            console.log('red is wrong');
+            return false;
+          }
+
+          if (data[pixelIndex + 2] != color.b) {
+            console.log('red is wrong');
+            return false;
+          }
+        }
+      }
+    }
+
+    return true;
+  }
+};
+
+
+// Thumbnails
+
+var thumbnails = {
+  container: document.querySelector('#thumbnails'),
+  canvas:    document.querySelector('#thumbnails canvas.thumbnail'),
+
+  init: function() {
+    thumbnails.clear();
+  },
+
+  clear: function() {
+    while (thumbnails.container.firstChild) {
+      thumbnails.container.removeChild(thumbnails.container.firstChild);
+    }
+  },
+
+  items: function() {
+    return thumbnails.container.querySelector('canvas.thumbnail');
+  },
+
+  create: function(compactData) {
+    var canvas = thumbnails.canvas.cloneNode();
+    imageRenderer.render(canvas, compactData, 2);
+    canvas.data = compactData;
+    canvas.optimizedData = dataOptimizer.optimizeData(compactData);
+    return canvas;
+  },
+
+  add: function(thumbnail) {
+    thumbnails.container.appendChild(thumbnail);
+  },
+}
+
+thumbnails.init();
 
 // Image Scanning
 
 var imageScanner = {
-  sourceCanvas:  document.querySelector('#image-scanner canvas.source'),
+  canvas:  document.querySelector('#image-scanner canvas.source'),
   previewCanvas: document.querySelector('#image-scanner canvas.preview'),
   fileSelector:  document.querySelector('#image-scanner input'),
-  sourceRadi: [],
-  previewRadi: [],
+  radi: [],
   data: null,
   optimizedData: null,
   bytes: null,
 
   init: function() {
-    imageScanner.sourceWidth   = imageScanner.sourceCanvas.width;
-    imageScanner.previewWidth  = imageScanner.previewCanvas.width;
+    imageScanner.width   = imageScanner.canvas.width;
 
-    imageScanner.sourceContext  = imageScanner.sourceCanvas.getContext('2d');
-    imageScanner.previewContext = imageScanner.previewCanvas.getContext('2d');
+    imageScanner.context  = imageScanner.canvas.getContext('2d');
 
-    imageScanner.sourceRadius  = imageScanner.sourceWidth / 2;
-    imageScanner.previewRadius = imageScanner.previewWidth / 2;
+    imageScanner.radius  = imageScanner.width / 2;
 
-    var sourceDeadRadius  = imageScanner.sourceRadius  * deadRadius;
-    var previewDeadRadius = imageScanner.previewRadius * deadRadius;
+    var sourceDeadRadius  = imageScanner.radius  * deadRadius;
 
     for (var pixel = 0; pixel < pixelCount; pixel++) {
-      imageScanner.sourceRadi[pixel]  = ((imageScanner.sourceRadius - sourceDeadRadius) / pixelCount) * pixel + sourceDeadRadius;
-      imageScanner.previewRadi[pixel] = ((imageScanner.previewRadius - previewDeadRadius) / pixelCount) * pixel + previewDeadRadius;
+      imageScanner.radi[pixel]  = ((imageScanner.radius - sourceDeadRadius) / pixelCount) * pixel + sourceDeadRadius;
     }
 
     imageScanner.fileSelector.addEventListener('change', function(e) {
@@ -196,32 +399,22 @@ var imageScanner = {
     return (step / resolution) * 2 * Math.PI;
   },
 
-  _sourceCoordinates: function(step, pixel) {
+  _coordinates: function(step, pixel) {
     var angle = imageScanner._angleForStep(step);
 
     return {
-      x: Math.cos(angle) * imageScanner.sourceRadi[pixel] + imageScanner.sourceRadius,
-      y: Math.sin(angle) * imageScanner.sourceRadi[pixel] + imageScanner.sourceRadius
-    }
-  },
-
-  _previewCoordinates: function(step, pixel) {
-    var angle = imageScanner._angleForStep(step);
-
-    return {
-      x: Math.cos(angle) * imageScanner.previewRadi[pixel] + imageScanner.previewRadius,
-      y: Math.sin(angle) * imageScanner.previewRadi[pixel] + imageScanner.previewRadius,
-      angle: angle
+      x: Math.cos(angle) * imageScanner.radi[pixel] + imageScanner.radius,
+      y: Math.sin(angle) * imageScanner.radi[pixel] + imageScanner.radius
     }
   },
 
   _colorAtCoordinates: function(x, y) {
-    return imageScanner.sourceContext.getImageData(x, y, 1, 1).data;
+    return imageScanner.context.getImageData(x, y, 1, 1).data;
   },
 
   _colorAtPixel: function(step, pixel) {
-    var coordinates  = imageScanner._sourceCoordinates(step, pixel);
-    var imageData    = imageScanner.sourceContext.getImageData(coordinates.x, coordinates.y, 1, 1).data;
+    var coordinates  = imageScanner._coordinates(step, pixel);
+    var imageData    = imageScanner.context.getImageData(coordinates.x, coordinates.y, 1, 1).data;
 
     return {
       x: coordinates.x,
@@ -245,23 +438,18 @@ var imageScanner = {
     imageScanner.data = [];
 
     imageScanner.optimizedData = null;
-    imageScanner.sourceContext.clearRect(0, 0, imageScanner.sourceWidth, imageScanner.sourceWidth);
-    imageScanner.previewContext.clearRect(0, 0, imageScanner.previewWidth, imageScanner.previewWidth);
+    imageScanner.context.clearRect(0, 0, imageScanner.width, imageScanner.width);
 
-    imageScanner.sourceContext.drawImage(img, 0, 0, imageScanner.sourceWidth, imageScanner.sourceWidth);
+    imageScanner.context.drawImage(img, 0, 0, imageScanner.width, imageScanner.width);
 
     imageScanner._scanImage(function(step, pixel, data) {
-      var previewCoordinates = imageScanner._previewCoordinates(step, pixel);
-
-      imageScanner.previewContext.fillStyle = 'rgb(' + data.r + ', ' + data.g + ', ' + data.b + ')';
-      imageScanner.previewContext.fillRect(previewCoordinates.x, previewCoordinates.y, 3, 3);
-
       imageScanner.data[step] = imageScanner.data[step] || [];
       imageScanner.data[step].push([data.r, data.g, data.b])
     });
 
-    imageScanner.optimizeData();
+    imageRenderer.render(imageScanner.previewCanvas, imageScanner.data, 4);
 
+    imageScanner.optimizedData = dataOptimizer.optimizeData(imageScanner.data);
   },
 
   scanURL: function(url) {
@@ -270,83 +458,8 @@ var imageScanner = {
     img.addEventListener('load', function() {
       imageScanner.scanImage(img);
     });
-  },
-
-  optimizeData: function() {
-    imageScanner.optimizedData = {};
-
-    var bytesPerStrip = pixelCount * 3;
-    var bytesPerBlock = 4 * bytesPerStrip;
-
-    imageScanner.optimizedData.buffer = new ArrayBuffer(resolution * bytesPerBlock);
-    var view = imageScanner.optimizedData.view = new Uint8Array(imageScanner.optimizedData.buffer);
-
-    var stepsBetweenStrips = resolution / 4;
-
-    var strips = [];
-    var stepIndex;
-    var pixelOffset;
-
-    for (var step = 0; step < resolution; step++) {
-
-      for (var strip = 0; strip < 4; strip++) {
-        stepIndex = step + (strip * stepsBetweenStrips);
-        if (stepIndex >= resolution) { stepIndex = stepIndex - resolution; }
-
-        for (var pixel = 0; pixel < pixelCount; pixel++) {
-          pixelOffset = (step * bytesPerBlock) + (strip * bytesPerStrip) + (pixel * 3);
-          view[pixelOffset + 0] = imageScanner.data[stepIndex][pixel][0];
-          view[pixelOffset + 1] = imageScanner.data[stepIndex][pixel][1];
-          view[pixelOffset + 2] = imageScanner.data[stepIndex][pixel][2];
-        }
-
-      }
-
-    }
-  },
-
-  verifyOptimizedData: function(data) {
-    var bytesPerStrip      = pixelCount * 3;
-    var bytesPerBlock      = 4 * bytesPerStrip;
-    var stepsBetweenStrips = resolution / 4;
-    var stepIndex;
-    var pixelIndex;
-    var color;
-
-    if (data.length != (resolution * bytesPerBlock)) {
-      console.log('data has wrong length');
-      return false;
-    }
-
-    for (var step = 0; step < resolution; step++) {
-      for (var strip = 0; strip < 4; strip++) {
-        stepIndex = step + (strip * stepsBetweenStrips);
-        if (stepIndex >= resolution) { stepIndex = stepIndex - resolution; }
-
-        for (var pixel = 0; pixel < pixelCount; pixel++) {
-          pixelIndex = step * bytesPerBlock + strip * bytesPerStrip + pixel * 3;
-          color = imageScanner._colorAtPixel(stepIndex, pixel);
-
-          if (data[pixelIndex] != color.r) {
-            console.log('red is wrong');
-            return false;
-          }
-
-          if (data[pixelIndex + 1] != color.g) {
-            console.log('red is wrong');
-            return false;
-          }
-
-          if (data[pixelIndex + 2] != color.b) {
-            console.log('red is wrong');
-            return false;
-          }
-        }
-      }
-    }
-
-    return true;
   }
+
 };
 
 imageScanner.init();
--- app/background.js
+++ app/background.js
@@ -1,7 +1,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('window.html', {
     'bounds': {
-      'width': 520,
+      'width': 640,
       'height': 700
     }
   });
